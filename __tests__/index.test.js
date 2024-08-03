9452b2219fde7559f62823d507ac1883cb4a9a25
visetin <visetin9@yandex.ru>
2024-08-03 20:03:08 +0300

    feat: start search implementation

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,65 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, it, expect } from '@jest/globals';
+import docs from '../__fixtures__/docs.json';
 import search from '../src/index.js';
 
-test('test stub', () => {
-    expect(search()).toEqual('hello search');
+describe('search by text', () => {
+    it('empty collection', () => {
+        const result = search([], 'None');
+
+        expect(result).toEqual([]);
+    });
+
+    it('empty text', () => {
+        const result = search(docs, '');
+
+        expect(result).toEqual([]);
+    });
+
+    it('non-exist words', () => {
+        const result = search(docs, 'None');
+
+        expect(result).toEqual([]);
+    });
+
+    it('exist word', () => {
+        const testCases = [
+            { text: 'shoot', result: ['doc1', 'doc2'] },
+            { text: 'straight', result: ['doc1'] },
+            { text: 'thing', result: ['doc2'] },
+            { text: 'shooter', result: ['doc3'] },
+        ];
+
+        testCases.forEach(({ text, result }) => {
+            const searchResult = search(docs, text);
+            expect(searchResult).toEqual(result);
+        });
+    });
+
+    it('exist word from sentence', () => {
+        const testCases = [
+            { text: 'had a pint', result: ['doc1'] },
+            { text: 'that thing', result: ['doc2'] },
+            { text: 'your shooter', result: ['doc3'] },
+            { text: 'Don\'t shoot', result: ['doc1', 'doc2'] },
+            { text: 'I can\'t', result: ['doc1', 'doc3'] }
+        ];
+
+        testCases.forEach(({ text, result }) => {
+            const searchResult = search(docs, text);
+            expect(searchResult).toEqual(result);
+        });
+    });
+
+    it('exist word case insensitive', () => {
+        const testCases = [
+            { text: 'ShooT', result: ['doc1', 'doc2'] },
+            { text: 'ShooteR', result: ['doc3'] },
+            { text: 'StraighT', result: ['doc1'] },
+        ];
+
+        testCases.forEach(({ text, result }) => {
+            const searchResult = search(docs, text);
+            expect(searchResult).toEqual(result);
+        });
+    });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-export default () => {
-    return 'hello search';
+import _ from 'lodash';
+
+/**
+ * @typedef {Object} SEDocument
+ * @property {string} id
+ * @property {string} text
+ */
+
+/**
+ * @param {string} rawWord
+ * @return {string}
+ */
+const normalizeWord = (rawWord) => {
+    const [word] = rawWord.match(/\w+/g);
+
+    if (!word) {
+        return '';
+    }
+
+    return word.toLowerCase();
 };
+
+/**
+ * @param {string} text
+ * @return {string[]}
+ */
+const parseTextToWords = (text) => {
+    return text.split(' ')
+        .map(normalizeWord)
+        .filter((word) => { return word; });
+};
+
+/**
+ * @param {string[]} haystack
+ * @param {string[]} needle
+ * @return {string[] | []}
+ */
+const findWordsIntersection = (haystack, needle) => {
+    return _.intersection(haystack, needle);
+};
+
+/**
+ * @param {SEDocument[]} docs
+ * @param {string} text
+ * @return {string[] | []}
+ */
+const search = (docs, text) => {
+    if (!docs.length || !text) {
+        return [];
+    }
+
+    const targetWords = parseTextToWords(text);
+
+    return docs.reduce((acc, { id, text: docText }) => {
+        const docWords = parseTextToWords(docText);
+        const intersectionWordsMap = findWordsIntersection(docWords, targetWords);
+
+        return intersectionWordsMap.length ? [...acc, id] : acc;
+    }, []);
+};
+
+export default search;
